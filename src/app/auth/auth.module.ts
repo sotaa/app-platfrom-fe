c807167567f52fb14ecb368c82ac7b25a7943579
Meitix <m.valizadeh@live.com>
2019-12-16 16:47:23 +0800

    Auth raw module added.

---
+++ src/app/auth/auth.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { ResetPasswordComponent } from './reset-password/reset-password.component';
+
+@NgModule({
+  declarations: [RegisterComponent, LoginComponent, ResetPasswordComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class AuthModule { }
