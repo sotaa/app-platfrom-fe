8a74d8e1d0cff08b641cd476d0e6b2cf86124b88
soheil najafi <soheilnajafimail2@gmail.com>
2020-02-15 18:22:11 +0330

    create form for application create

--- src/app/dashboard/application/add-or-update-application/add-or-update-application.component.ts
+++ src/app/dashboard/application/add-or-update-application/add-or-update-application.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  Output,
+  EventEmitter,
+  ViewChild
+} from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-add-or-update-application',
+
   templateUrl: './add-or-update-application.component.html',
   styleUrls: ['./add-or-update-application.component.scss']
 })
 export class AddOrUpdateApplicationComponent implements OnInit {
+  @ViewChild('f') form: NgForm;
+  name: string;
+  address: string;
+  active: boolean;
+  isLoading: boolean;
+  @Output() closeAppForm = new EventEmitter<boolean>();
+  constructor() {}
 
-  constructor() { }
+  ngOnInit() { }
 
-  ngOnInit() {
+  emitCloseAppForm() {
+    this.closeAppForm.emit();
   }
-
 }
