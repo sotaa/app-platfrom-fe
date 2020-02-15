eb59c1d2438a377db4af2c1b7c897b81dadb98e0
soheil najafi <soheilnajafimail2@gmail.com>
2020-02-15 18:20:42 +0330

    import formModule

--- src/app/dashboard/application/application.module.ts
+++ src/app/dashboard/application/application.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { AddOrUpdateApplicationComponent , ListApplicationComponent} from '.';
 import { Routes, RouterModule } from '@angular/router';
@@ -17,6 +18,7 @@ const routes: Routes = [
   ],
   imports: [
     CommonModule,
+    FormsModule,
     RouterModule.forChild(routes),
     TranslateModule
   ],
