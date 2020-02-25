8c85dd2c02d11f1c78b23d4654b0617c43e527a2
soheil najafi <soheilnajafimail2@gmail.com>
2020-02-25 12:21:38 +0330

    create paymentplan class

--- src/app/payment-plans/models/payment-plan.interface.ts
+++ src/app/payment-plans/models/payment-plan.interface.ts
@@ -6,3 +6,12 @@ export interface IPaymentPlan {
   isActive: boolean;
   description?: string;
 }
+
+export class PaymentPlan implements IPaymentPlan {
+  id?: number;
+  name: string;
+  price: number;
+  dateRange: number;
+  isActive: boolean;
+  description?: string;
+}
