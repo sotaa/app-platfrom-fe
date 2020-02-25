c800b21aeaa865344574c6b6c84627d0d5cbe681
soheil najafi <soheilnajafimail2@gmail.com>
2020-02-25 12:24:07 +0330

    create manageing plan service

--- src/app/payment-plans/payment-plans.service.ts
+++ src/app/payment-plans/payment-plans.service.ts
@@ -1,13 +1,44 @@
 import { Injectable } from '@angular/core';
 import { IPaymentPlan } from './models';
 import { from, timer } from 'rxjs';
+import { AuthHttpClient } from '../auth/auth-http-client.service';
+import { environment } from 'src/environments/environment';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PaymentPlansService {
 
-  constructor() { }
+  constructor(private authHttpClient:AuthHttpClient) { }
+
+  createPaymentPlan( data: IPaymentPlan ){
+    return this.authHttpClient.post<IPaymentPlan>(environment.identityUrls.createPaymentPlan,data).pipe(
+      map(res => res)
+    );
+  }
+
+  editPaymentPlan( data: IPaymentPlan, id ){
+    return this.authHttpClient.put<IPaymentPlan>(environment.identityUrls.createPaymentPlan + `/${id}`,data).pipe(
+      map(res => res)
+    );
+  }
+
+  getPaymentPlans(){
+    return this.authHttpClient.get<IPaymentPlan[]>(environment.identityUrls.readPaymentPlans).pipe(
+      map( res => res)
+    );
+  }
+
+  getPaymentPlan(id){
+    return this.authHttpClient.get<IPaymentPlan>(environment.identityUrls.readPaymentPlan.concat(id)).pipe(
+      map( res => res)
+    );
+  }
+
+  deletePaymentPlan( id ){
+    return this.authHttpClient.delete(environment.identityUrls.deletePaymentPlan.concat(id)).pipe(map(res=>res))
+  }
 
   buy(plan: IPaymentPlan) {
     /** Mock observabel for testing */
