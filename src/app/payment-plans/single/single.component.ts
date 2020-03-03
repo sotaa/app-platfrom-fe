2b44500deb3d742b43bcdddf44ec7363cfef22c7
soheil najafi <soheilnajafimail2@gmail.com>
2020-03-03 10:20:37 +0330

    handle buy function

--- src/app/payment-plans/single/single.component.ts
+++ src/app/payment-plans/single/single.component.ts
@@ -7,28 +7,28 @@ import { PaymentPlansService } from '../payment-plans.service';
   templateUrl: './single.component.html',
   styleUrls: ['./single.component.scss']
 })
-export class SingleComponent implements OnInit
-{
+export class SingleComponent implements OnInit {
   @Output() deletePlan = new EventEmitter<boolean>();
   @Output() editPlan = new EventEmitter<boolean>();
   @Input() plan: IPaymentPlan;
   @Input() hasEditPlanPermission: boolean;
   @Input() hasDeletePlanPermission: boolean;
-  constructor(private service: PaymentPlansService) { }
+  constructor(private paymentPlansService: PaymentPlansService) { }
 
-  ngOnInit(){
+  ngOnInit() {
   }
 
-  deletePaymentPlan( id ){
-    this.deletePlan.emit( id );
+  deletePaymentPlan(id) {
+    this.deletePlan.emit(id);
   }
-  editPaymentPlan( id ){
-    this.editPlan.emit( id );
+  editPaymentPlan(id) {
+    this.editPlan.emit(id);
   }
 
-  buy() {
-    this.service.buy(this.plan).subscribe(result => {
-      console.log(` you bought the plan ${this.plan.name}`)
+  buy(id) {
+    this.paymentPlansService.buyPlan(id).subscribe((res: any) => {
+      const url = res.completionEndpoint
+      window.open(url, '_blank');
     });
   }
 
