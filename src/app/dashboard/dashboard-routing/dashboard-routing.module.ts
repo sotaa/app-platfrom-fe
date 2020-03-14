3f9929ec46cd3d1e7ad8e83b10770df5a04db7f9
soheil najafi <soheilnajafimail2@gmail.com>
2020-03-14 11:20:28 +0330

    add role route

--- src/app/dashboard/dashboard-routing/dashboard-routing.module.ts
+++ src/app/dashboard/dashboard-routing/dashboard-routing.module.ts
@@ -4,10 +4,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { LayoutComponent } from '../layout/layout.component';
 
 const routes: Routes = [
-  {path: '', component: LayoutComponent, children: [
-    {path: '', pathMatch: 'full', loadChildren: '../application/application.module#ApplicationModule'},
-    {path: 'payment-plans', loadChildren: '../../payment-plans/payment-plans.module#PaymentPlansModule'},
-  ]},
+  {
+    path: '', component: LayoutComponent, children: [
+      { path: '', pathMatch: 'full', loadChildren: '../application/application.module#ApplicationModule' },
+      { path: 'payment-plans', loadChildren: '../../payment-plans/payment-plans.module#PaymentPlansModule' },
+      { path: 'roles', loadChildren: '../../roles/roles.module#RolesModule' },
+    ]
+  },
 ];
 
 
