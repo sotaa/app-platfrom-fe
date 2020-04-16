fbec27edd96e64621c9e172523099bdb6aceb911
soheil najafi <soheilnajafimail2@gmail.com>
2020-04-16 18:32:25 +0430

    create methods for getting and setting refresh token

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,3 +1,4 @@
+import { ITokenPair } from './models/token.interface';
 import { Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
 import { HttpClient } from '@angular/common/http';
@@ -14,6 +15,10 @@ import { IUser } from '../user/models';
   providedIn: 'root'
 })
 export class AuthService {
+
+  refreshToken: string
+  interval;
+  user;
 
   constructor(private http: HttpClient, private authStore: Store<IAuthState>) {
     const savedUserData = this.getSavedAuthResult();
@@ -51,36 +56,11 @@ export class AuthService {
       .pipe(tap(res => res));
   }
 
-  logout() {
-    // localStorage.clear();
-    sessionStorage.clear();
-    return this.authStore.dispatch(new ChangeUser(undefined));
-  }
-
   private getSavedAuthResult() {
     const localAuthResult = sessionStorage.getItem('authResult') ||
       sessionStorage.getItem('authResult');
     return localAuthResult ? JSON.parse(localAuthResult) : undefined;
   }
-
-  // getCurrentUser(): Observable<IAuthResult> {
-  //   return new Observable(observer => {
-  //     const localUserStr = this.authResult ||
-  //     sessionStorage.getItem('authResult') ||
-  //     sessionStorage.getItem('authResult');
-
-  //   if (!localUserStr) {
-  //      observer.next(null);
-  //      return;
-  //   }
-
-  //   if (typeof localUserStr === 'string') {
-  //     this.authResult = JSON.parse(localUserStr);
-  //   }
-
-  //    observer.next(this.authResult.user);
-  //   })
-  // }
 
   getCurrentUser() {
     return this.authStore.select((a: any) => a.authState.authResult);
@@ -89,6 +69,51 @@ export class AuthService {
   private setAuthResult(user: IAuthResult) {
     this.authStore.dispatch(new ChangeUser(user));
     sessionStorage.setItem('authResult', JSON.stringify(user));
+
+    // Get and Set new token in session storage
+    //  clearInterval(this.interval);
+    //  this.interval =  setInterval(() => {
+    //     console.log("GO")
+    //     this.getAndSetNewToken();
+    //   }, 5000);
+  }
+
+  logout() {
+    clearInterval(this.interval);
+    sessionStorage.clear();
+    return this.authStore.dispatch(new ChangeUser(undefined));
+  }
+
+  async getAndSetNewToken() {
+    // get refresh token from session storage
+    await this.getCurrentUserRefreshToken();
+    // get new tokens from server with refreshToken
+    const res = await this.getNewTokenFromServer(this.refreshToken).toPromise();
+    // create new user object with new tokens
+    await this.createNewUserObjWithNewTokens(res);
+    // set new tokens in session storage
+     this.setAuthResult(this.user);
+  }
+
+ private getCurrentUserRefreshToken() {
+   this.getCurrentUser().subscribe(res => {
+     res ? (res.refreshToken ? this.refreshToken = res.refreshToken : this.refreshToken=null) : null;
+    })
+  }
+
+ private getNewTokenFromServer(refreshToken: string) {
+    return this.http
+    .get<any>(environment.identityUrls.refreshToken.concat(refreshToken))
+      .pipe(tap(res => res));
+  }
+
+  private createNewUserObjWithNewTokens(tokens: ITokenPair) {
+    this.getCurrentUser().subscribe(res => {
+      res ?
+     (this.user = res,
+      this.user.token = tokens.token,
+      this.user.refreshToken = tokens.refreshToken) : null
+    })
   }
 
   rememberCurrentUser() {
@@ -114,3 +139,23 @@ export class AuthService {
     }));
   }
 }
+
+
+// getCurrentUser(): Observable<IAuthResult> {
+//   return new Observable(observer => {
+//     const localUserStr = this.authResult ||
+//     sessionStorage.getItem('authResult') ||
+//     sessionStorage.getItem('authResult');
+
+//   if (!localUserStr) {
+//      observer.next(null);
+//      return;
+//   }
+
+//   if (typeof localUserStr === 'string') {
+//     this.authResult = JSON.parse(localUserStr);
+//   }
+
+//    observer.next(this.authResult.user);
+//   })
+// }
